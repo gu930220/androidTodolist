de8e115035fd44d61d281382d371aa1575586604
ikmo <gu930220@hanmail.net>
2020-08-31 01:26:45 +0900

    200831 firebase connect

--- app/src/main/java/com/imkoo/todolist/MainActivity.kt
+++ app/src/main/java/com/imkoo/todolist/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.imkoo.todolist
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Paint
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.imkoo.todolist.databinding.ActivityMainBinding
 import com.imkoo.todolist.databinding.ItemTodoBinding
 
+
 class MainActivity : AppCompatActivity() {
+    val RC_SIGN_IN = 1000;
+
     private lateinit var binding: ActivityMainBinding
 
-    private val data = arrayListOf<Todo>()
+    private val viewModel: MainViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,31 +36,165 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        data.add(Todo("숙제", false))
-        data.add(Todo("청소", false))
+        //로그인이 안되있을때
+        if (FirebaseAuth.getInstance().currentUser ==null) {
+            login()
+        }
+
 
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        binding.recyclerView.adapter = TodoAdapter(data)
+        binding.recyclerView.adapter = TodoAdapter(
+            emptyList(),
+            onClickDeleteIcon = {
+                viewModel.deleteTodo(it)
+            },
+            onClickItem = {
+                viewModel.toggleTodo(it)
+            }
+        )
+
+        binding.addButton.setOnClickListener {
+            val todo = Todo(binding.editText.text.toString())
+            viewModel.addTodo(todo)
+        }
+
+        // 관찰 UI 업데이트
+        viewModel.todoLiveData.observe(this, Observer{
+            (binding.recyclerView.adapter as TodoAdapter).setData(it)
+        });
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+            } else {
+                // 로그인 실패
+                finish()
+            }
+        }
+    }
+
+    fun login(){ //로그인 창창
+       val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build())
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    fun logout(){ // 로그아웃 하기
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                login()
+            }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.action_log_out -> {
+                logout()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
 
-data class Todo(val text: String, var isDone: Boolean)
+data class Todo(
+    val text: String,
+    var isDone: Boolean = false
+)
 
-class TodoAdapter(private val myDataset: List<Todo>) :
+class TodoAdapter(
+    private var myDataset: List<Todo>,
+    val onClickDeleteIcon: (todo: Todo) -> Unit,
+    val onClickItem: (todo: Todo) -> Unit
+) :
     RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
 
     class TodoViewHolder(val binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): TodoAdapter.TodoViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TodoAdapter.TodoViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_todo, parent, false)
         return TodoViewHolder(ItemTodoBinding.bind(view))
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val todo = myDataset[position]
         holder.binding.todoText.text = myDataset[position].text
+
+        if (todo.isDone) {
+            /*holder.binding.todoText.paintFlags = holder.binding.todoText.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG*/
+            holder.binding.todoText.apply {
+                paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                setTypeface(null, Typeface.ITALIC)
+            }
+        } else {
+            holder.binding.todoText.apply {
+                paintFlags = 0
+                setTypeface(null, Typeface.NORMAL)
+            }
+
+        }
+        holder.binding.deleteImageView.setOnClickListener {
+            onClickDeleteIcon.invoke(todo)
+
+        }
+        holder.binding.root.setOnClickListener {
+            onClickItem.invoke(todo)
+        }
     }
 
     override fun getItemCount() = myDataset.size
+
+    fun setData(newData: List<Todo>) {
+        myDataset = newData
+        notifyDataSetChanged() //data가 바뀔때마다 setData 메서드를 호출
+    }
+}
+
+class MainViewModel: ViewModel() {
+    val todoLiveData = MutableLiveData<List<Todo>>()
+
+    private val data = arrayListOf<Todo>()
+
+    fun toggleTodo(todo: Todo) {
+        todo.isDone = !todo.isDone
+        todoLiveData.value = data
+    }
+
+    fun addTodo(todo:Todo) { // editText의 값을 데이터에 추가
+        data.add(todo)
+
+        todoLiveData.value = data
+    }
+
+    fun deleteTodo(todo: Todo) {
+        data.remove(todo)
+        todoLiveData.value = data
+    }
 }
